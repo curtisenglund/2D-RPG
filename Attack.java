1e0cf045340276ecbe0f5ae883971e8e15fb5bdc
Harpreet Sangha <100447473+hasangha04@users.noreply.github.com>
2022-05-09 10:14:33 -0700

    Add files via upload

---
+++ Attack.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Attack
+{
+    public static void main (String [] args)
+    {
+        //Scanner input = new Scanner(System.in);
+        Enemy jerry = new Enemy();
+        Hero hero = new Hero();
+        hero.setHealthHero();
+        jerry.setHealthEnemy();
+        System.out.println(jerry.getHealthEnemy());
+    }
+}
---
+++ Enemy.java
@@ -0,0 +1,14 @@
+public class Enemy
+{
+    private int healthE = 50;
+    private int damageE = 50;
+
+    public int getHealthEnemy()
+    {
+        return healthE;
+    }
+
+    public void setHealthEnemy() {
+        this.healthE = healthE-damageE;
+    }
+}
